65ac476660d9d90fb8f22574e98e0edb6bf99528
antoniotorresolmo <77279203+antoniotorresolmo@users.noreply.github.com>
2023-09-25 17:32:40 +0200

    rest

---
+++ testapp/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from datetime import datetime
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
+
+def get_example(request):
+    response = {'request': {'time': datetime.now().isoformat(),
+                            'method': request.method,
+                            'path': request.path,
+                            'params': request.GET,
+                            'headers': dict(request.headers)}}
+    return JsonResponse(response, safe=False, json_dumps_params={'indent': 2})
+
+@csrf_exempt
+def post_example(request):
+    response = {'request': {'time': datetime.now().isoformat(),
+                'method': request.method,
+                'path': request.path,
+                'params': request.GET,
+                'headers': dict(request.headers),
+                'body': request.body.decode()}}
+    return JsonResponse(response, safe=False, json_dumps_params={'indent': 2})
